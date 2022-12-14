32b62e05cf5d93918a44e269a856ade35319d5e6
hardyn <104727569+hardynstyle@users.noreply.github.com>
2022-12-14 12:23:05 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,66 +1,38 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Dec 14 08:23:25 2022
-
-@author: hardyn
-"""
-
-'''
-Camera Example
-==============
-
-This example demonstrates a simple use of the camera. It shows a window with
-a buttoned labelled 'play' to turn the camera on and off. Note that
-not finding a camera, perhaps because gstreamer is not installed, will
-throw an exception during the kv language processing.
-
-'''
-
-# Uncomment these lines to see all the messages
-# from kivy.logger import Logger
-# import logging
-# Logger.setLevel(logging.TRACE)
-
-from kivy.app import App
+import datetime
 from kivy.lang import Builder
-from kivy.uix.boxlayout import BoxLayout
-import time
-Builder.load_string('''
-<CameraClick>:
+from kivy.app import App
+
+kv = """
+#:import XCamera kivy.garden.xcamera.XCamera
+
+FloatLayout:
     orientation: 'vertical'
-    Camera:
-        id: camera
-        resolution: (640, 480)
-        play: False
-    ToggleButton:
-        text: 'Play'
-        on_press: camera.play = not camera.play
-        size_hint_y: None
-        height: '48dp'
-    Button:
-        text: 'Capture'
-        size_hint_y: None
-        height: '48dp'
-        on_press: root.capture()
-''')
+
+    XCamera:
+        id: xcamera
+        on_picture_taken: app.picture_taken(*args)
+
+    BoxLayout:
+        orientation: 'vertical'
+        size_hint: 1, None
+        height: sp(50)
+
+        Button:
+            text: 'Set landscape'
+            on_release: xcamera.force_landscape()
+
+        Button:
+            text: 'Restore orientation'
+            on_release: xcamera.restore_orientation()
+"""
 
 
-class CameraClick(BoxLayout):
-    def capture(self):
-        '''
-        Function to capture the images and give them the names
-        according to their captured time and date.
-        '''
-        camera = self.ids['camera']
-        timestr = time.strftime("%Y%m%d_%H%M%S")
-        camera.export_to_png("IMG_{}.png".format(timestr))
-        print("Captured")
-
-
-class TestCamera(App):
-
+class CameraApp(App):
     def build(self):
-        return CameraClick()
+        return Builder.load_string(kv)
 
+    def picture_taken(self, obj, filename):
+        print('Picture taken and saved to {}'.format(filename))
 
-TestCamera().run()
+if __name__ == '__main__':
+    CameraApp().run()
